446381512a4bd3d9fa07b9ea2abe5d55290e0309
Alexander Neumann <alexander@bumpern.de>
2017-05-01 12:40:01 +0200

    swift: use backend.Layout, fix tests

--- src/restic/backend/swift/swift_test.go
+++ src/restic/backend/swift/swift_test.go
@@ -3,39 +3,71 @@ package swift_test
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"restic"
 	"time"
 
+	"restic/debug"
 	"restic/errors"
 
 	"restic/backend/swift"
 	"restic/backend/test"
-	. "restic/test"
-
-	swiftclient "github.com/ncw/swift"
 )
 
 //go:generate go run ../test/generate_backend_tests.go
 
 func init() {
-	if TestSwiftServer == "" {
-		SkipMessage = "swift test server not available"
+	// only use minimal data
+	test.MinimalData = true
+
+	cfg := swift.Config{}
+
+	for _, val := range []struct {
+		s   *string
+		env string
+	}{
+		// v2/v3 specific
+		{&cfg.UserName, "OS_USERNAME"},
+		{&cfg.APIKey, "OS_PASSWORD"},
+		{&cfg.Region, "OS_REGION_NAME"},
+		{&cfg.AuthURL, "OS_AUTH_URL"},
+
+		// v3 specific
+		{&cfg.Domain, "OS_USER_DOMAIN_NAME"},
+		{&cfg.Tenant, "OS_PROJECT_NAME"},
+		{&cfg.TenantDomain, "OS_PROJECT_DOMAIN_NAME"},
+
+		// v2 specific
+		{&cfg.TenantID, "OS_TENANT_ID"},
+		{&cfg.Tenant, "OS_TENANT_NAME"},
+
+		// v1 specific
+		{&cfg.AuthURL, "ST_AUTH"},
+		{&cfg.UserName, "ST_USER"},
+		{&cfg.APIKey, "ST_KEY"},
+
+		// Manual authentication
+		{&cfg.StorageURL, "OS_STORAGE_URL"},
+		{&cfg.AuthToken, "OS_AUTH_TOKEN"},
+
+		{&cfg.DefaultContainerPolicy, "SWIFT_DEFAULT_CONTAINER_POLICY"},
+	} {
+		if *val.s == "" {
+			*val.s = os.Getenv(val.env)
+		}
+	}
+
+	cfg.Container = os.Getenv("RESTIC_TEST_SWIFT_CONTAINER")
+	if cfg.Container == "" {
+		SkipMessage = "RESTIC_TEST_SWIFT_CONTAINER unset, skipping test"
 		return
 	}
 
-	// Generate random container name to allow simultaneous test
-	// on the same swift backend
-	containerName := fmt.Sprintf(
-		"restictestcontainer_%d_%d",
-		time.Now().Unix(),
-		rand.Uint32(),
-	)
+	// use a unique prefix
+	rand.Seed(time.Now().UnixNano())
+	cfg.Prefix = fmt.Sprintf("travis-%s-%d", os.Getenv("TRAVIS_BUILD_ID"), rand.Int63())
 
-	cfg := swift.Config{
-		Container:  containerName,
-		StorageURL: TestSwiftServer,
-		AuthToken:  TestSwiftToken,
-	}
+	debug.Log("opening swift repository at %#v", cfg)
 
 	test.CreateFn = func() (restic.Backend, error) {
 		be, err := swift.Open(cfg)
@@ -60,17 +92,15 @@ func init() {
 	}
 
 	test.CleanupFn = func() error {
-		client := swiftclient.Connection{
-			StorageUrl: TestSwiftServer,
-			AuthToken:  TestSwiftToken,
+		type deleter interface {
+			Delete() error
 		}
-		objects, err := client.ObjectsAll(containerName, nil)
+
+		be, err := swift.Open(cfg)
 		if err != nil {
 			return err
 		}
-		for _, o := range objects {
-			client.ObjectDelete(containerName, o.Name)
-		}
-		return client.ContainerDelete(containerName)
+
+		return be.(deleter).Delete()
 	}
 }
