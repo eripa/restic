fbd59c51ae2b25a0c15c15b21be39fc16940ca9f
Alexander Neumann <alexander@bumpern.de>
2017-05-26 12:00:27 +0200

    swift: Add ApplyEnvironment

--- src/restic/backend/swift/config.go
+++ src/restic/backend/swift/config.go
@@ -1,6 +1,7 @@
 package swift
 
 import (
+	"os"
 	"restic/errors"
 	"strings"
 )
@@ -52,4 +53,44 @@ func ParseConfig(s string) (interface{}, error) {
 	}
 
 	return cfg, nil
+}
+
+// ApplyEnvironment saves values from the environment to the config.
+func ApplyEnvironment(cfg interface{}) error {
+	c := cfg.(*Config)
+	for _, val := range []struct {
+		s   *string
+		env string
+	}{
+		// v2/v3 specific
+		{&c.UserName, "OS_USERNAME"},
+		{&c.APIKey, "OS_PASSWORD"},
+		{&c.Region, "OS_REGION_NAME"},
+		{&c.AuthURL, "OS_AUTH_URL"},
+
+		// v3 specific
+		{&c.Domain, "OS_USER_DOMAIN_NAME"},
+		{&c.Tenant, "OS_PROJECT_NAME"},
+		{&c.TenantDomain, "OS_PROJECT_DOMAIN_NAME"},
+
+		// v2 specific
+		{&c.TenantID, "OS_TENANT_ID"},
+		{&c.Tenant, "OS_TENANT_NAME"},
+
+		// v1 specific
+		{&c.AuthURL, "ST_AUTH"},
+		{&c.UserName, "ST_USER"},
+		{&c.APIKey, "ST_KEY"},
+
+		// Manual authentication
+		{&c.StorageURL, "OS_STORAGE_URL"},
+		{&c.AuthToken, "OS_AUTH_TOKEN"},
+
+		{&c.DefaultContainerPolicy, "SWIFT_DEFAULT_CONTAINER_POLICY"},
+	} {
+		if *val.s == "" {
+			*val.s = os.Getenv(val.env)
+		}
+	}
+	return nil
 }
